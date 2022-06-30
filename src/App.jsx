ca6d67ce089b3fa7ed65336a65b0895ba601716e
kokkorin <104617188+kokkorin@users.noreply.github.com>
2022-06-30 17:26:29 +0900

    min-heightを追加

--- src/App.jsx
+++ src/App.jsx
@@ -3,8 +3,8 @@ import "./styles.css";
 
 export const App = () => {
   const [todoText, setTodoText] = useState("");
-  const [incompleteTodos, setIncompleteTodos] = useState(["", ""]);
-  const [completeTodos, setCompleteTodos] = useState([""]);
+  const [incompleteTodos, setIncompleteTodos] = useState([]);
+  const [completeTodos, setCompleteTodos] = useState([]);
   const onChangeTodoText = (event) => setTodoText(event.target.value);
   const onClickAdd = () => {
     if (todoText === "") return;
